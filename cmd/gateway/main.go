8ef95ada719fcce40c8c49553c4bf4df93b0a847
Eric Williams <ewilliams0305@gmail.com>
2023-11-27 10:30:19 -0500

    created static page

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -3,32 +3,44 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 
 	"github.com/carlmjohnson/gateway"
 )
 
+type Link struct {
+	Title       string
+	Description string
+	Image       string
+	Link        string
+}
+
+var links map[string][]Link = map[string][]Link{
+	"Links": {
+		{Title: "SOURCE CODE", Description: "contibute to bungolo", Image: "github-mark-white.png", Link: "https://github.com/bungolo-dev"},
+	},
+}
+
 func main() {
 
 	port := flag.Int("port", -1, "specify a port to use http rather than AWS Lambda")
 	flag.Parse()
+
+	home := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "public, max-age=300")
+		tmpl := template.Must(template.ParseFiles("../../public/index.html"))
+		tmpl.Execute(w, links)
+	}
+
 	listener := gateway.ListenAndServe
 	portStr := ""
 	if *port != -1 {
 		portStr = fmt.Sprintf(":%d", *port)
 		listener = http.ListenAndServe
-		http.Handle("/", http.FileServer(http.Dir("./public")))
+		http.HandleFunc("/", home)
 	}
 
-	http.Handle("/api/home", &home{})
-
 	log.Fatal(listener(portStr, nil))
-}
-
-type home struct{}
-
-func (h *home) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Cache-Control", "public, max-age=300")
-	w.Write([]byte("This is my home page"))
 }
